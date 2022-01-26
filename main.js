d9f15142d6b24af46fedf034cadf105351c6b9b4
AyaanMittal1 <84359433+AyaanMittal1@users.noreply.github.com>
2022-01-25 18:53:19 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,22 @@ Webcam.set({
      '<img id="image2" src="'+data_uri+'"/>';
 } );}
 console.log(ml5.version);
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/fAQwFi0sK/model.json",is_model_loaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/tYQGaiCGv/model.json",is_model_loaded);
 function is_model_loaded(){
     console.log("model loaded");
+}
+function recongnize(){
+    image1=document.getElementById("image2");
+    classifier.classify(image1,displayPic);
+}
+function displayPic(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("name").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=100*(results[0].confidence.toFixed(4))+"%";
+    }
+
 }
